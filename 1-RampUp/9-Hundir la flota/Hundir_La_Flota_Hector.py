2e02d36f6f5ec0ecdf58e0280fedabce4a71029c
hey-hector <hectoryustos@gmail.com>
2026-03-10 18:17:41 +0100

    chan

---
+++ 1-RampUp/9-Hundir la flota/Hundir_La_Flota_Hector.py
@@ -0,0 +1,149 @@
+#Como hacerlo así va a ser un poco lío, vamos a hacer los pasos 1 a 1 y ya después los meteré en clases y funciones.
+#Comencemos con el tablero. Debería haber 2 versiones de tablero que deberían estar en la misma clase:
+    #- 1 por cada jugador: el que veo yo y el que ve mi contrincante:
+    
+#TABLERO 1: MI TABLERO. Crear un Tablero como una lista de listas de 10x10 que contenga caracteres espacio: " " 
+#*de internet*
+tablero1 = [] #se empieza con una lista vacía y le añadiremos filas (i) y columnas (j) de un rango de 10
+for i in range(10):
+    fila = [] #se crea la lista vacía de filas
+    for j in range(10): #este bucle 'for' está dentro para que haya una columna por cada fila - para que estén asociadas (o eso creo)
+        fila.append(" ") #para cada columna (y en consecuencia, cada fila) se le añade el espacio - ¿al final de la lista?
+    tablero1.append(fila) #a la lista vacía 'tablero', se le añade el bucle entero al final
+    
+print(tablero1)
+    
+#Así tenemos la lista "tablero1" de 10 filas y 1o columnas (10 listas con 10 elementos) con espacios en blanco
+
+#TABLERO 2: al principio será igual. Luego ya no 
+
+tablero2 = [] #se empieza con una lista vacía y le añadiremos filas (i) y columnas (j) de un rango de 10
+for i in range(10):
+    fila = [] #se crea la lista vacía de filas
+    for j in range(10): #este bucle 'for' está dentro para que haya una columna por cada fila - para que estén asociadas (o eso creo)
+        fila.append(" ") #para cada columna (y en consecuencia, cada fila) se le añade el espacio - ¿al final de la lista?
+    tablero2.append(fila) #a la lista vacía 'tablero', se le añade el bucle entero al final
+    
+print(tablero2)
+
+#Así tenemos las listas "tablero1" y "tablero2" de 10 filas y 1o columnas (10 listas con 10 elementos) con espacios en blanco
+
+#PASO 2: meter los barcos en cada tablero:
+# * 4 barcos de 1 posición de eslora
+# * 3 barcos de 2 posiciones de eslora
+# * 2 barcos de 3 posiciones de eslora
+# * 1 barco de 4 posiciones de eslora
+#lo voy a hacer como en Mini Hundir La Flota porque no sé cómo hacerlo para que elija los barcos de manera aleatoria
+
+#TABLERO 1:
+#tablero1[coordenada_i][coordenada_j]:
+#Barco 4 posiciones:
+tablero1[1][0] = "B"
+tablero1[1][1] = "B"
+tablero1[1][2] = "B"
+tablero1[1][3] = "B"
+#Barco_1 3 posiciones:
+tablero1[3][3] = "B"
+tablero1[4][3] = "B"
+tablero1[5][3] = "B"
+#Barco_2 3 posiciones:
+tablero1[8][4] = "B"
+tablero1[8][5] = "B"
+tablero1[8][6] = "B" 
+#Barco_1 2 posiciones: 
+tablero1[6][9] = "B"
+tablero1[7][9] = "B"
+#Barco_2 2 posiciones:
+tablero1[7][3] = "B"
+tablero1[7][4] = "B"
+#Barco_3 2 posiciones:
+tablero1[0][6] = "B"
+tablero1[1][6] = "B"
+#Barco_1 4 posiciones:
+tablero1[5][5] = "B"
+#Barco_2 4 posiciones:
+tablero1[9][6] = "B"
+#Barco_3 4 posiciones:
+tablero1[9][9] = "B"
+#Barco_4 4 posiciones:
+tablero1[6][4] = "B"
+#Esto significa que la coordenada_i es la lista en la que está el elemento y la coordenada_j será la posición dentro de esa lista
+print(tablero1)
+
+#TABLERO 2: esto  lo tiene que rellenar la máquina. Pero vamos a imaginar que yo soy la máquina también, ¿quién sino?
+# Al final lo hacemos tablero[coordenada_i][coordenada_j]:
+#Barco 4 posiciones:
+tablero2[8][0] = "B"
+tablero2[8][1] = "B"
+tablero2[8][2] = "B"
+tablero2[8][3] = "B"
+#Barco_1 3 posiciones:
+tablero2[4][3] = "B"
+tablero2[5][3] = "B"
+tablero2[6][3] = "B"
+#Barco_2 3 posiciones:
+tablero2[1][4] = "B"
+tablero2[2][4] = "B"
+tablero2[3][4] = "B" 
+#Barco_1 2 posiciones: 
+tablero2[6][6] = "B"
+tablero2[7][6] = "B"
+#Barco_2 2 posiciones:
+tablero2[9][3] = "B"
+tablero2[9][4] = "B"
+#Barco_3 2 posiciones:
+tablero2[5][0] = "B"
+tablero2[6][0] = "B"
+#Barco_1 4 posiciones:
+tablero2[1][1] = "B"
+#Barco_2 4 posiciones:
+tablero2[9][6] = "B"
+#Barco_3 4 posiciones:
+tablero2[6][9] = "B"
+#Barco_4 4 posiciones:
+tablero2[4][6] = "B"
+#Esto significa que la coordenada_i es la lista en la que está el elemento y la coordenada_j será la posición dentro de esa lista
+print(tablero2)
+
+#PASO 3: turnos
+
+#JUGADOR 1: el jugador 1 juega en el tablero 2. Vamos a meter un ciclo "while" para que la máquina nos pida inputs y vaya imprimiendo hasta que fallemos:
+while tablero1[][] == "B" or tablero2[][] == "B": #aquí hay que poner la condición de que no haya Bs en uno de los tableros
+    while True: #while 'B' in [item for sublist in tablero1 for item in sublist] or 'B' in [item for sublist in tablero2 for item in sublist]:
+        coordenada_i2 = int(input("Tu turno jugador 1. Meta la fila deseada. Valores 0-9:")) #fila (Nº de lista dentro de la lista)
+        coordenada_j2 = int(input("Meta la columna deseada. Valores 0-9:")) #columna (Nº de elemento dentro de la lista)
+        print(coordenada_i2, coordenada_j2) 
+        print(tablero2[coordenada_i2][coordenada_j2]) #este comando print es para que muestre lo que hay en esa posición: si he acertado, que salga "B"; sino, saldrá " "
+        if tablero2[coordenada_i2][coordenada_j2] == "B": 
+            tablero2[coordenada_i2][coordenada_j2] = "x"
+            print("Tocado en", coordenada_i2, coordenada_j2, "¡¡Te toca de nuevo!!")
+            print(tablero2)
+        elif tablero2[coordenada_i2][coordenada_j2] == " ":
+            tablero2[coordenada_i2][coordenada_j2] = "o"
+            print("Agua. Sorry :( Turno del jugador 2")
+            print(tablero2)
+            break
+        elif tablero2[coordenada_i2][coordenada_j2] == "o" or tablero2[coordenada_i2][coordenada_j2] == "x":
+            print("Tío, aquí ya has disparado. Try Again Next Time :p Turno del jugador 2")
+            print(tablero2)
+            break
+#JUGADOR 2: el jugador 2 juega en el tablero 1. Vamos a meter un ciclo "while" para que la máquina nos pida inputs y vaya imprimiendo hasta que fallemos:
+    while True:
+        coordenada_i1 = int(input("Tu turno jugador 2. Meta la fila deseada. Valores 0-9:")) #fila (Nº de lista dentro de la lista)
+        coordenada_j1 = int(input("Meta la columna deseada. Valores 0-9:")) #columna (Nº de elemento dentro de la lista)
+        print(coordenada_i1, coordenada_j1) 
+        print(tablero1[coordenada_i1][coordenada_j1]) #este comando print es para que muestre lo que hay en esa posición: si he acertado, que salga "B"; sino, saldrá " "
+        if tablero1[coordenada_i1][coordenada_j1] == "B": 
+            tablero1[coordenada_i1][coordenada_j1] = "x"
+            print("Tocado en", coordenada_i1, coordenada_j1,"¡¡Te toca de nuevo!!")
+            print(tablero1)
+        elif tablero1[coordenada_i1][coordenada_j1] == " ":
+            tablero1[coordenada_i1][coordenada_j1] = "o"
+            print("Agua. Sorry :( Turno del jugador 1")
+            print(tablero1)
+            break
+        elif tablero1[coordenada_i1][coordenada_j1] == "o" or tablero1[coordenada_i1][coordenada_j1] == "x":
+            print("Tío, aquí ya has disparado. Try Again Next Time :p Turno del jugador 1")
+            print(tablero1)
+            break
+#"pass" para que empiece el bucle otra vez. Como la condición del bucle no es "while True", no es necesario. Ya está la condición puesta en el "while"
